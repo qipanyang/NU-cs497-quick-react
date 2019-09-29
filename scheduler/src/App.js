e3ab50bf4d4e28d2a2b7dc7066d4e1c7f91fa13b
Panyang Qi <qipanyang@126.com>
2019-09-29 11:03:30 -0500

    slice4

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
 import 'rbx/index.css';
 import {Button, Container, Title} from 'rbx';
+import React, {useState, useEffect} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -26,36 +26,36 @@ import {Button, Container, Title} from 'rbx';
 // }
 
 //variables
-const schedule = {
-  "title": "CS courses for 2019 Fall",
-  "courses":[
-    {
-      "id": "F:CS497",
-      "title": "Rapid Prototype",
-      "meets": "MWF 13:00-13:50"
-    },
-    {
-      "id": "F:CS325",
-      "title": "AI Programming",
-      "meets": "MWF 11:00-11:50"
-    },
-    {
-      "id": "F:EE475",
-      "title": "ML",
-      "meets": "M 17:00-19:50"
-    },
-    {
-      "id": "F:EE332",
-      "title": "Computer Vision",
-      "meets": "MWF 13:00-13:50"
-    }
-  ]
-};
+// const schedule = {
+//   "title": "CS courses for 2019 Fall",
+//   "courses":[
+//     {
+//       "id": "F:CS497",
+//       "title": "Rapid Prototype",
+//       "meets": "MWF 13:00-13:50"
+//     },
+//     {
+//       "id": "F:CS325",
+//       "title": "AI Programming",
+//       "meets": "MWF 11:00-11:50"
+//     },
+//     {
+//       "id": "F:EE475",
+//       "title": "ML",
+//       "meets": "M 17:00-19:50"
+//     },
+//     {
+//       "id": "F:EE332",
+//       "title": "Computer Vision",
+//       "meets": "MWF 13:00-13:50"
+//     }
+//   ]
+// };
 const terms = {F:'Fall', W: "Winter", S: "Spring"};
 
 //component
 const Banner = props => (
-  <Title>{props.title}</Title>
+  <Title>{ props.title || '[loading...]' }</Title>
 );
 const CourseList = ({courses}) => (
   <Button.Group>
@@ -65,7 +65,8 @@ const CourseList = ({courses}) => (
 const Course = ({course}) => (
   // <div>
   <Button>
-    {getCourseTerm(course)} {getCourseDept(course)} {getCourseNum(course)}: {course.title}
+    {getCourseTerm(course)} CS {getCourseNum(course)}: {course.title}
+    {/* {getCourseTerm(course)} {getCourseDept(course)} {getCourseNum(course)}: {course.title} */}
   </Button>
   //</div>
 );
@@ -74,18 +75,33 @@ const Course = ({course}) => (
 
 //function 
 const getCourseTerm = course => (terms[course.id.charAt(0)]);
-const getCourseNum = course => (course.id.slice(4,7));
+const getCourseNum = course => (course.id.slice(1,4));
 const getCourseDept = course => (course.id.slice(2,4));
 
 
 // App
-const App = () => (
-  <Container> 
-  <div>
-    <Banner title = {schedule.title} />
-    <CourseList courses = {schedule.courses} /> 
-  </div>
-  </Container>
-);
+const App = () => {
+  const [schedule, setSchedule] = useState ({title: '', courses: []});
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  useEffect ( () => {
+    const fetchSchedule = async ( )=> {
+      const response = await fetch(url);
+      if (!response.ok) throw response;
+      const json = await response.json();
+      setSchedule(json);
+    }
+      fetchSchedule();
+    }, []
+  )
+  
+  return(
+    <Container> 
+    <div>
+      <Banner title = {schedule.title} />
+      <CourseList courses = {schedule.courses} /> 
+    </div>
+    </Container>
+  );
+};
 
 export default App;
