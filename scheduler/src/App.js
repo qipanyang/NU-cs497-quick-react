63f1a97ba2cdd45cc0992c76b952f7719b39986c
Panyang Qi <qipanyang@126.com>
2019-09-29 20:13:53 -0500

    slice5 don't know why stuck at course1.includes

--- scheduler/src/App.js
+++ scheduler/src/App.js
@@ -52,31 +52,89 @@ import React, {useState, useEffect} from 'react';
 //   ]
 // };
 const terms = {F:'Fall', W: "Winter", S: "Spring"};
-
-//component
-const Banner = props => (
-  <Title>{ props.title || '[loading...]' }</Title>
-);
-const CourseList = ({courses}) => (
-  <Button.Group>
-    { courses.map(course => <Course key={course.id} course={course} />) } 
-  </Button.Group>
-);
-const Course = ({course}) => (
-  // <div>
-  <Button>
-    {getCourseTerm(course)} CS {getCourseNum(course)}: {course.title}
-    {/* {getCourseTerm(course)} {getCourseDept(course)} {getCourseNum(course)}: {course.title} */}
-  </Button>
-  //</div>
-);
-
-
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
 //function 
 const getCourseTerm = course => (terms[course.id.charAt(0)]);
 const getCourseNum = course => (course.id.slice(1,4));
-const getCourseDept = course => (course.id.slice(2,4));
+// const getCourseDept = course => (course.id.slice(2,4));
+const buttonColor = selected => (selected ? 'button is-success is-selected': 'button');
+const useSelection = () =>
+{
+  const [selected, setSelected] = useState([]);
+  const toggle = (x) =>
+    {setSelected(selected.includes(x) ? selected.filter(y=>y !== x) : [x].concat(selected))};
+  return [selected, toggle];
+};
+const daysOverlap = (days1, days2) => (
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+const courseConflict = (course1, course2) => (
+  course1 !== course2 && getCourseTerm(course1) === getCourseTerm(course2) && timeConflict(course1, course2)
+);
+const hasConflict = (course, selected) =>(
+  selected.some(selection => courseConflict(course, selection))
+);
+
+//component
+const Banner = ({title}) => (
+  <Title>{ title || '[loading...]' }</Title>
+);
+const CourseList = ({courses}) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, toggle] = useSelection();
+  const termCourses = courses.filter(course => term === getCourseTerm(course));
+return(
+  <React.Fragment>
+    <TermSelector state={{term, setTerm}}/>
+  <div className="buttons">
+    { termCourses.map(course => <Course key={course.id} course={course} state={{selected, toggle}} />) } 
+  </div>
+  </React.Fragment>
+);
+};
+const Course = ({course, state}) => (
+  <button className={buttonColor(state.selected.includes(course))} 
+  onClick={()=>state.toggle(course)}
+  disabled={hasConflict(course, state.selected)}
+  >
+    {getCourseTerm(course)} CS {getCourseNum(course)}: {course.title}
+  </button>
+);
+const TermSelector = ({state}) =>  (
+    <div className = "field has-addons">
+      {Object.values(terms).map(value => 
+      <button key={value} className={buttonColor(value===state.term)} onClick={()=>state.setTerm(value)}> 
+        {value} 
+      </button>)}
+    </div> 
+);
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: schedule.courses.map(addCourseTimes)
+});
 
 
 // App
@@ -88,20 +146,24 @@ const App = () => {
       const response = await fetch(url);
       if (!response.ok) throw response;
       const json = await response.json();
-      setSchedule(json);
+      setSchedule(addScheduleTimes(json));
     }
       fetchSchedule();
     }, []
   )
   
   return(
-    <Container> 
-    <div>
+    <div className="container">
       <Banner title = {schedule.title} />
       <CourseList courses = {schedule.courses} /> 
     </div>
-    </Container>
   );
 };
 
+
+
 export default App;
+
+
+
+
